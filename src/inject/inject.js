4e08ac0b456b8eddd08235ebe888c30b0651c336
Jordan David Evans <163066340+JordanDavidEvans@users.noreply.github.com>
2025-12-15 12:18:48 +1000

    Refactor scripts for readability

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,76 +1,74 @@
-chrome.extension.onMessage.addListener(function(req, sender, respond) {
-	console.log("inject.js: Message Received! ["+req+"]");
-   if (req === 'ScriptRunning') {
-       respond('true');
-   } else if (req === 'StopScript') {
-   	   clearTimeout(newPageTimeout);
-   	   console.log("inject.js: StopScript received.");
-   	   respond('true');
-   }
+chrome.extension.onMessage.addListener((req, sender, respond) => {
+  console.log("inject.js: Message Received! [" + req + "]");
+  if (req === "ScriptRunning") {
+    respond("true");
+  } else if (req === "StopScript") {
+    clearTimeout(newPageTimeout);
+    console.log("inject.js: StopScript received.");
+    respond("true");
+  }
 });
 
 // Used to scale the random time generated for switching pages
-var clickFactor = 1;
+let clickFactor = 1;
 
-var googleSearch = false;
+let googleSearch = false;
 if (/^https?:\/\/www\.google\.com\/search/.test(window.location)) {
-	//console.log(">>Google Search Page Detected<<");
-	googleSearch = true;
-	clickFactor = 0.4;
+  googleSearch = true;
+  clickFactor = 0.4;
 }
 
 // Disable autocomplete on page so accounts aren't automatically logged into
-var inputnodes = document.getElementsByTagName('input');    
-for(var i=0;i<inputnodes.length;i++){       
-	inputnodes[i].setAttribute('autocomplete','blah');
-	inputnodes[i].setAttribute('name',' ');
+const inputnodes = document.getElementsByTagName("input");
+for (let i = 0; i < inputnodes.length; i++) {
+  inputnodes[i].setAttribute("autocomplete", "blah");
+  inputnodes[i].setAttribute("name", " ");
 }
 
-var formnodes = document.getElementsByTagName("form");    
-for(var i=0;i<formnodes.length;i++){                    
-	formnodes[i].setAttribute('autocomplete','blah');
-} 
+const formnodes = document.getElementsByTagName("form");
+for (let i = 0; i < formnodes.length; i++) {
+  formnodes[i].setAttribute("autocomplete", "blah");
+}
 
-var linksOnPage = document.getElementsByTagName('a');
+const linksOnPage = document.getElementsByTagName("a");
 
 // Filter the list of links
-var links = [];
-for(var i=0; i<linksOnPage.length; ++i) {
-	// Remove any link that does not begin with http or https or is a Bookmark
-	if (/^http/.test(linksOnPage[i]) && !/#\S*$/.test(linksOnPage[i])) {
-		if (googleSearch && /^https?:\/\/[^\/]*google[^\/]*\//.test(linksOnPage[i])) {
-			//console.log("Google Search and Google URL Detected, skipping: ["+linksOnPage[i]+"]");
-		} else {
-			links.push(linksOnPage[i]);
-			//console.log("Link: ["+linksOnPage[i]+"]");
-		}
-	} else {
-		//console.log("Bad Link: ["+linksOnPage[i]+"]");
-	}
+const links = [];
+for (let i = 0; i < linksOnPage.length; ++i) {
+  const link = linksOnPage[i];
+  // Remove any link that does not begin with http or https or is a Bookmark
+  if (/^http/.test(link) && !/#\S*$/.test(link)) {
+    if (googleSearch && /^https?:\/\/[^/]*google[^/]*\//.test(link)) {
+      // Skip Google URLs when on a Google search page
+    } else {
+      links.push(link);
+    }
+  }
 }
 
-console.log("Links Found: ["+links.length+"] of ["+linksOnPage.length+"]");
+console.log("Links Found: [" + links.length + "] of [" + linksOnPage.length + "]");
 
 if (links.length > 0) {
-	var selectedLink = randInt(0, links.length-1);
+  const selectedLink = randInt(0, links.length - 1);
 
-	var maxTime = Math.round(scriptOptions.maxTimeBetweenClicks*clickFactor);
-	var delay = randInt(Math.floor(maxTime*scriptOptions.timeBetweenClicksVariance*clickFactor), maxTime)*1000;
-	console.log("Using Delay: ["+scriptOptions.maxTimeBetweenClicks+"]");
-	console.log("New URL: ["+links[selectedLink]+"] in "+delay+"ms");
-	var newPageTimeout = setTimeout(
-		function() {
-	        window.location = links[selectedLink];
-	    }
-	, delay); 
+  const maxTime = Math.round(scriptOptions.maxTimeBetweenClicks * clickFactor);
+  const delay = randInt(Math.floor(maxTime * scriptOptions.timeBetweenClicksVariance * clickFactor), maxTime) * 1000;
+  console.log("Using Delay: [" + scriptOptions.maxTimeBetweenClicks + "]");
+  console.log("New URL: [" + links[selectedLink] + "] in " + delay + "ms");
+  var newPageTimeout = setTimeout(
+    function () {
+      window.location = links[selectedLink];
+    },
+    delay
+  );
 } else {
-	console.log("!!No links found.");	
+  console.log("!!No links found.");
 }
 
 // Attempt to disable "Are you sure you want to leave" popups
 window.onbeforeunload = null;
 window.onunload = null;
 
-function randInt (min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+function randInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
